a1ab7d41ed59975766d9e9e2be5e624326a47021
KalimullinDinar <dinaritis@yandex.ru>
2014-02-12 23:41:56 +0400

    Revert 7f754ed..97c23cc
    This rolls back to commit 7f754eda5cc881af25330d65b1eda22fc839c823.

--- django20140210/task/Site/Site/settings.py
+++ django20140210/task/Site/Site/settings.py
@@ -121,7 +121,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'math2',
-    'car',
     # Uncomment the next line to enable the admin:
      'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
--- django20140210/task/Site/car/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, include, url
-
-urlpatterns = patterns('car.views',
-url(r'^$', 'index'),
-url(r'^new/(?P<a>.*)$', 'new'),
-)
